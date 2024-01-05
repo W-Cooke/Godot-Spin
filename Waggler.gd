4b2b76f099c0e0faf600aaf4171cf06039e7856c
Will Cooke <william.cooke89@gmail.com>
2024-01-05 23:18:02 +0000

    implementing waggle detection
    Heh, waggle.

---
+++ Waggler.gd
@@ -0,0 +1,38 @@
+extends Node
+
+#TODO: attach waggle script to check for waggle
+var waggle_array = []
+var is_spinning : bool = false
+var is_waggling : bool = false
+signal waggle_signal
+
+func waggle():
+	var stick_rotation: Vector2 = Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y), Input.get_joy_axis(0, JOY_AXIS_RIGHT_X))
+	if stick_rotation.y > 0.2 or stick_rotation.y < -0.2:
+		waggle_array.push_front(stick_rotation.y)
+	if waggle_array.size() > 21:
+		# same hacky method to get ride of one null value at beginning of array ¯\_(ツ)_/¯
+		waggle_array.resize(20)
+		print(is_spinning)
+		waggle_array.resize(1)
+		#TODO: calculate threshold for detecting waggle, implement waggle script
+		#TODO: look up array methods to figure this out
+
+func _process(_delta):
+	waggle()
+
+func reset_bools():
+	is_waggling = false
+
+func signal_bools():
+	if is_waggling:
+		waggle_signal.emit("YES")
+	else:
+		waggle_signal.emit("NO")
+
+func _on_spinner_spinning_signal(string):
+	# ensures that waggle script won't execute when stick is spinning
+	if string == "NO":
+		is_spinning = false
+	else:
+		is_spinning = true
--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node
 
-@onready var spin_label = $VBoxContainer/Label2
+@onready var spin_label = $VBoxContainer/HBoxContainer/Label2
+@onready var waggle_label = $VBoxContainer/HBoxContainer2/Label3
 
 var spinning_clockwise = false
 var spinning_anticlockwise = false
@@ -10,3 +11,7 @@ func _process(_delta):
 
 func _on_spinner_spinning_signal(string):
 	spin_label.text = string
+
+
+func _on_waggler_waggle_signal(string):
+	waggle_label.text = string
